c99deef25de5c18332e5805e5870f9c66c0c3393
Luciana is YammYcoding@freenode:6667 <luphp@yahoo.com.br>
2022-04-02 02:11:04 -0300

    Modal e Forms

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,20 @@
+import logoImg from '../../assets/logo.svg';
+import { Container, Content } from './styles';
+
+interface HeaderProps {
+  onOpenNewTransactionModal: () => void;
+}
+
+export function Header( {onOpenNewTransactionModal}: HeaderProps) {
+
+  return (
+    <Container>
+      <Content>
+        <img src={logoImg} alt="lucontas" />
+        <button onClick={onOpenNewTransactionModal}>
+          nova transacao
+        </button>
+      </Content>
+    </Container>
+  )
+}
