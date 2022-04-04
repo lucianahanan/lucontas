04ba48b70feded124265b26dffd01f9033693d42
Luciana is YammYcoding@freenode:6667 <luphp@yahoo.com.br>
2022-04-04 05:01:04 -0300

    Modal e Forms com .env

--- src/services/api.js
+++ src/services/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 console.log(process.env.REACT_APP_API_URL);
 
 export const api = axios.create({
-  baseURL: 'http://localhost:3000/api',
+//  baseURL: 'http://localhost:3000/api',
+  baseURL: process.env.REACT_APP_API_URL,
   headers: {}
 })
