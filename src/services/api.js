32017d2e5f62d0f58d3227266881d023ca2c6256
Luciana is YammYcoding@freenode:6667 <luphp@yahoo.com.br>
2022-04-04 05:38:03 -0300

    Modal e Forms com .env

--- src/services/api.js
+++ src/services/api.js
@@ -1,9 +1,17 @@
 import axios from 'axios';
 
+let currentUrl = (window.location.href === process.env.REACT_APP_API_URL ? process.env.REACT_APP_API_URL : 'https://lucontas-lucianahanan.vercel.app/');
+
+if( window.location.href === 'http://localhost:3000/') {
+  currentUrl = process.env.REACT_APP_API_URL;
+}
+
 console.log(process.env.REACT_APP_API_URL);
+console.log(currentUrl);
 
 export const api = axios.create({
-//  baseURL: 'http://localhost:3000/api',
-  baseURL: process.env.REACT_APP_API_URL,
+// baseURL: 'http://localhost:3000/api',
+// baseURL: process.env.REACT_APP_API_URL,
+  baseURL: currentUrl,
   headers: {}
 })
